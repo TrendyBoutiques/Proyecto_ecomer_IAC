4f3777a293b2adf22eb6eaeede55b9b0d59af06e
daniponcee <daniponcee57@gmail.com>
2025-12-03 18:41:27 -0500

    Actualizacion

--- iac/main.tf
+++ iac/main.tf
@@ -5,15 +5,18 @@ terraform {
       version = "~> 5.0"
     }
   }
+
   backend "s3" {
     bucket = "terraform-state-proyecto-ecommerce"
-    key    = "terraform.tfstate"       
-    region = "us-east-2"               
+    key    = "terraform.tfstate"
+    region = "us-east-2"
   }
 }
+
 provider "aws" {
   region = "us-east-2"
 }
+
 provider "aws" {
   alias  = "waf_region"
   region = "us-east-1"
